dc085cfdb5d343bca0a61107a7cb460b5e9cd0f6
mariza <marizadima@yahoo.com>
2016-11-18 16:40:49 +0000

    Initial Commit

---
+++ ARMovieProj/aRMovie/src/main/obj/local/x86/objs/cpufeatures/cpu-features.o.d
@@ -0,0 +1,5 @@
+./obj/local/x86/objs/cpufeatures/cpu-features.o: \
+  /Users/mariza/Library/Android/sdk/ndk-bundle/sources/android/cpufeatures/cpu-features.c \
+  /Users/mariza/Library/Android/sdk/ndk-bundle/sources/android/cpufeatures/cpu-features.h
+
+/Users/mariza/Library/Android/sdk/ndk-bundle/sources/android/cpufeatures/cpu-features.h:
